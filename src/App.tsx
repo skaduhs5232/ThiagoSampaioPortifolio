bca418b8d55ae566b019e28ee8630b53eebfda53
skaduhs5232 <thiagooliveira1039@gmail.com>
2024-11-15 21:08:34 -0300

    confetti

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Confetti from "react-confetti";
 import {
   Main,
   Timeline,
@@ -14,17 +15,35 @@ import StarsCanvas from './components/Stars';
 
 function App() {
   const [mode, setMode] = useState<string>('dark');
+  const [showConfetti, setShowConfetti] = useState<boolean>(true);
 
   const handleModeChange = () => {
     setMode((prevMode) => (prevMode === 'dark' ? 'light' : 'dark'));
   };
-  
+
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-  }, []);
+    // Configura o confete para desaparecer após alguns segundos
+    const timer = setTimeout(() => {
+      setShowConfetti(false); // Desativa o confete após 4.8 segundos
+    }, 10000);
+
+    return () => clearTimeout(timer); // Limpa o timer ao desmontar o componente
+  }, []); // Executa apenas uma vez no carregamento
 
   return (
     <div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'} relative`}>
+      {/* Confetti */}
+      {showConfetti && (
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          numberOfPieces={400} // Número de peças de confete
+          gravity={0.7} // Controla a gravidade para confetes mais lentos
+          initialVelocityY={9} // Ajusta a posição inicial vertical para "mais embaixo"
+          recycle={false} // Impede que os confetes sejam reciclados
+        />
+      )}
+
       {/* StarsCanvas como fundo fixo */}
       <div className="stars-background">
         <StarsCanvas mode={mode} /> {/* Passa o modo para StarsCanvas */}
